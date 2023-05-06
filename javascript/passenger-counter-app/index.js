3dd1494ef584750203b34cd65aa1ae3a069b3847
Gökşen Ayar <goksenns@gmail.com>
2023-05-06 20:21:23 +0300

    start-js

---
+++ javascript/passenger-counter-app/index.js
@@ -0,0 +1,22 @@
+// document.getElementById("count-el").innerText = 5
+
+// let firstBatch= 5
+// let secondBatch= 7
+
+// let count= firstBatch + secondBatch
+
+// console.log(count)
+
+let bonusPoints = 50
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+
+
+console.log(bonusPoints)
