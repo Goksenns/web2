72cfebad1b2681c548abf91e668f18a1de7457fc
Gökşen Ayar <goksenns@gmail.com>
2023-05-08 02:19:30 +0300

    functions

--- javascript/passenger-counter-app/index.js
+++ javascript/passenger-counter-app/index.js
@@ -7,15 +7,43 @@
 
 // console.log(count)
 
-let bonusPoints= 50
+// let bonusPoints= 50
 
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
 
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
 
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    count+=1
+    countEl.textContent=count
+}
+
+function save() {
+
+     let countStr= count + " - "
+     saveEl.textContent += countStr
+     countEl.textContent=0
+     count=0
+
+}
+
+
+ 
+
+
+
+
+
+
 
 
