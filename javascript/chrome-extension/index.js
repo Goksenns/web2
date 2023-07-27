953a1e9ac2019366d59b50c0c90fb347cbedbce4
Gökşen Ayar <goksenns@gmail.com>
2023-07-27 21:45:13 +0300

    learning template strings

--- javascript/chrome-extension/index.js
+++ javascript/chrome-extension/index.js
@@ -1,25 +1,29 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-
-
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    renderLeads()
-})
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
 
 function renderLeads() {
-let listItems = ""
-for (let i=0; i<myLeads.length; i++){
-    listItems += "<li>" + myLeads[i] + "</li>"
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    listItems += 
+    `<li>
+          <a target='_blank' href='${myLeads[i]}'>
+             ${myLeads[i]}
+          </a>
+    </li>`;
 
     //another way of innerHTML
     // const li= document.createElement("li")
     // li.textContent = myLeads[i]
     // ulEl.append(li)
+  }
+  ulEl.innerHTML = listItems;
 }
-ulEl.innerHTML = listItems
-}
-
