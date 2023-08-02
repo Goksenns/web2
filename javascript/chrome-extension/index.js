022f91a578530cbf9df453d6fc9a01df3626dd04
Gökşen Ayar <goksenns@gmail.com>
2023-08-02 18:20:31 +0300

    truthy and falsy values

--- javascript/chrome-extension/index.js
+++ javascript/chrome-extension/index.js
@@ -1,24 +1,22 @@
-let myLeads = '["www.awesome.com"]';
-
-myLeads = JSON.parse(myLeads)
-
-myLeads.push("www.eren.com")
-
-myLeads = JSON.stringify(myLeads)
-
-console.log(typeof myLeads)
-
-
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+localStorage.clear()
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+console.log(leadsFromLocalStorage)
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+
+localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
   renderLeads();
+
+  console.log(localStorage.getItem("myLeads"))
 });
 
 function renderLeads() {
---
+++ javascript/truthy and falsy values/index.js
@@ -0,0 +1,37 @@
+// let credits=""
+
+// if(credits){
+//     console.log("Let's play")
+// } else{
+//     console.log("Sorry, you have no credits")
+// }
+
+//TRUTHY
+// FALSY
+
+// false
+// 0 
+// ""
+// null -> how you as a developer signalize emptiness
+// undefined -> how Javascript signalize emptiness
+// NaN
+
+
+// NULL:
+
+// let currentViewers = null
+
+// currentViewers = ["jane","nick"]
+
+// currentViewers = null
+
+// if(currentViewers){
+//     console.log("we have viewers")
+// }
+
+
+// UNDEFİNED:
+
+let currentViewers =["jane"]
+
+console.log(currentViewers[5])
