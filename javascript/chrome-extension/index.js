a4916acfb70014f3b3df9f25e3fafaef4fa65325
Gökşen Ayar <goksenns@gmail.com>
2023-08-06 20:00:34 +0300

    create tab button

--- javascript/chrome-extension/index.js
+++ javascript/chrome-extension/index.js
@@ -5,11 +5,25 @@ const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn")
 
 if(leadsFromLocalStorage){
   myLeads = leadsFromLocalStorage
   render(myLeads)
 }
+
+tabBtn.addEventListener("click", function(){
+
+  chrome.tabs.query({active : true, currentWindow: true}, function(tabs){
+    
+  // console.log(tabs[0].url)
+  myLeads.push(tabs[0].url)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
+  })
+
+})
+
 
 function render(leads) {
   let listItems = "";
